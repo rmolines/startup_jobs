4984b7ec813cc4585c331e8a35f34025a913b70e
rmolines <rafaelmismael@gmail.com>
2023-04-11 14:35:48 -0300

    asdf

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -13,19 +13,19 @@ export function NavHeader() {
 			<nav className="container max-w-5xl h-full flex mx-auto items-center justify-between gap-x-4">
 				<Link
 					href="/"
-					className="h-full w-fit flex items-center gap-x-2 basis-1/3 justify-start"
+					className="h-full w-fit flex items-center gap-x-2 md:basis-1/3 justify-start"
 				>
 					<Image
 						src={logo}
 						alt="logo"
 						className="h-2/5 w-auto my-auto"
 					/>
-					<h1 className="text-white hidden sm:block text-xl whitespace-nowrap font-medium">
+					<h1 className="text-white hidden md:block text-xl whitespace-nowrap font-medium">
 						VagasEmStartups.com
 					</h1>
 				</Link>
 
-				<div className="h-full flex items-center justify-end gap-x-8 basis-1/3">
+				<div className="h-full flex items-center justify-end gap-x-8 md:basis-2/3">
 					<Link
 						href="/"
 						className="text-white text-lg hover:text-stone-200 transition-colors font-light"
@@ -44,7 +44,7 @@ export function NavHeader() {
 					>
 						Blog
 					</Link> */}
-					<div className="relative flex max-w-lg w-full">
+					<div className="relative flex max-w-xs w-full">
 						<HiSearch className="absolute inset-y-0 h-full left-2 text-slate-400" />
 						<input
 							className="bg-stone-50 mx-auto w-full pl-7 rounded-lg border-2 border-stone-300 px-2 py-0.5"
